9eeebe0c2725ebaed7c1ac8e955c98b9b4c4264e
Dylan <38665633+dylie@users.noreply.github.com>
2021-04-01 19:05:45 +0200

    Updating footer info, phone number and mail addres

--- src/components/map.vue
+++ src/components/map.vue
@@ -5,7 +5,7 @@
       <div class="container-fluid">
         <div class="bottom-section">
           <div class="row">
-            <div class="col-md-6 p-0">
+            <div class="col-md-12 p-0">
               <div class="address-bar">
                 <div>
                   <ul class="footer-style">
@@ -14,7 +14,7 @@
                         <img :src='"../assets/images/locate.png"' alt="locate">
                       </div>
                       <div class="footer-address">
-                        <a href="#">B-17 ABC Center,  Near Temple, USA  123456</a>
+                        <a>Our Team Is Located Across The Globe. </a>
                       </div>
                     </li>
                     <li>
@@ -22,7 +22,7 @@
                         <img :src='"../assets/images/telephone.png"' alt="telephone">
                       </div>
                       <div class="footer-address">
-                        <a href="#">122-456-7890  /  124-789-5690</a>
+                        <a href="tel:888-700-3846" class="hover-style-link">+1.888.700.ETHO</a>
                       </div>
                     </li>
                     <li>
@@ -30,24 +30,14 @@
                         <img :src='"../assets/images/fotter-email.png"' alt="fotter-email">
                       </div>
                       <div class="footer-address">
-                        <a href="#">support@compnay.com</a>
-                      </div>
-                    </li>
-                    <li>
-                      <div class="footer-icon">
-                        <img :src='"../assets/images/fax.png"' alt="">
-                      </div>
-                      <div class="footer-address">
-                        <a href="#">123-456  /  456-789</a>
+                        <a href="mailto:admin@ether1.org" class="hover-style-link">admin@ether1.org</a>
                       </div>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div class="col-md-6 p-0">
-              <iframe :src='"https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d35391.48685150366!2d72.77537852898678!3d21.18930639680146!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1530610361925"'  class="md-height"></iframe>
-            </div>
+           
           </div>
         </div>
       </div>
