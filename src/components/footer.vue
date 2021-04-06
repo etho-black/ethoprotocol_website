7cefac35efabd71abbc694bd37642986f8e2b1c0
Dev-JamesR <admin@ether1.org>
2021-04-05 17:03:39 -0700

    Adding Social Media Links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,9 +1,27 @@
 <template>
 <div>
   <!-- Footer Section start -->
+
+              
+     
+        
   <div class="copyright-section index-footer bg-light">
+    <center>
+      <a href="https://www.facebook.com/Ether1Project/" target="_blank" class="social-link"> <i class="fa fa-facebook"></i> </a>
+      <a href="https://twitter.com/Ether1Official" target="_blank" class="social-link"> <i class="fa fa-twitter" style="padding:10px;"></i> </a>
+      <a href="https://www.instagram.com/ether1official/" target="_blank" class="social-link"> <i class="fa fa-instagram" style="padding:10px;"></i> </a>
+      <a href="https://github.com/Ether1Project" target="_blank" class="social-link"> <i class="fa fa-github" style="padding:10px;"></i> </a>
+      <a href="https://play.google.com/store/apps/details?id=com.ether1.trust.wallet" target="_blank" class="social-link"> <i class="fa fa-android" style="padding:10px;"></i> </a>
+      <a href="https://t.me/Ether_1" target="_blank" class="social-link"> <i class="fa fa-telegram" style="padding:10px;"></i> </a>
+      <a href="https://old.reddit.com/r/etho1/" target="_blank" class="social-link"> <i class="fa fa-reddit" style="padding:10px;"></i> </a>
+      <a href="https://medium.com/@Ether1Official" target="_blank" class="social-link"> <i class="fa fa-medium" style="padding:10px;"></i> </a>
+      <a href="https://www.youtube.com/c/ether1" target="_blank" class="social-link"> <i class="fa fa-youtube" style="padding:10px;"></i> </a>
+      <a href="https://discordapp.com/invite/MFn9Tmz" target="_blank" class="social-link"> <i class="fa fa-discord" style="padding:10px;"></i> </a>
+    </center>
     <p>© 2021 The ETHO Protocol | Ether-1 All Rights Reserved.</p>
   </div>
+
+  
   <!-- Footer Section End -->
   <!-- Tap on Top -->
   <div class="tap-top">
