bf45a7e59406e393223d2cf95f569e91e98cbdd0
Dev-JamesR <admin@ether1.org>
2021-04-05 17:20:38 -0700

    Text & Wallet Updates

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -10,8 +10,8 @@
         <b-nav-item style="cursor: pointer;" @click="openStaking">Staking</b-nav-item>
 
         <b-nav-item-dropdown right-alignment text="wallets" class="nav-link">
-          <b-dropdown-item class="nav-link" :href='"https://apps.apple.com/us/app/nextshore-ether-1-wallet/id1521802485#?platform=iphone"'>Wallet (iOS)</b-dropdown-item>
-          <b-dropdown-item class="nav-link" :href='"https://play.google.com/store/apps/details?id=app.web.nextshore_ether1&hl=en&gl=US"'>Wallet (Android)</b-dropdown-item>
+          <b-dropdown-item class="nav-link" :href='"https://apps.apple.com/us/app/nextshore-ether-1-wallet/id1521802485#?platform=iphone"' target="_blank">Wallet (iOS)</b-dropdown-item>
+          <b-dropdown-item class="nav-link" :href='"https://play.google.com/store/apps/details?id=app.web.nextshore_ether1&hl=en&gl=US"' target="_blank">Wallet (Android)</b-dropdown-item>
           <b-dropdown-item class="nav-link" :href='"https://wallet.ether1.org/"' target="_blank">Web Wallet</b-dropdown-item>
           <b-dropdown-item class="nav-link" :href='"https://github.com/Ether1Project/Ether1DesktopWallet"' target="_blank">Desktop Wallet</b-dropdown-item>
         </b-nav-item-dropdown>
