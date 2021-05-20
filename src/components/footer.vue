12b87c062d7a41aab354e7344f1074fdea1ee199
Dev-JamesR <admin@ether1.org>
2021-05-19 19:59:22 -0700

    Updating Builds

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -7,13 +7,13 @@
       <a href="https://www.facebook.com/Ether1Project/" target="_blank" class="social-link"> <i class="fa fa-facebook"></i> </a>
       <a href="https://twitter.com/EthoProtocol" target="_blank" class="social-link"> <i class="fa fa-twitter" style="padding:10px;"></i> </a>
       <a href="https://www.instagram.com/ether1official/" target="_blank" class="social-link"> <i class="fa fa-instagram" style="padding:10px;"></i> </a>
-      <a href="https://github.com/Ether1Project" target="_blank" class="social-link"> <i class="fa fa-github" style="padding:10px;"></i> </a>
+      <a href="https://github.com/Ether1Project" target="_blank" class="social-link"> <i class="fa fa-github-square" style="padding:10px;"></i> </a>
       <a href="https://play.google.com/store/apps/details?id=com.ether1.trust.wallet" target="_blank" class="social-link"> <i class="fa fa-android" style="padding:10px;"></i> </a>
       <a href="https://t.me/Ether_1" target="_blank" class="social-link"> <i class="fa fa-telegram" style="padding:10px;"></i> </a>
       <a href="https://old.reddit.com/r/etho1/" target="_blank" class="social-link"> <i class="fa fa-reddit" style="padding:10px;"></i> </a>
       <a href="https://ethoprotocol.medium.com/" target="_blank" class="social-link"> <i class="fa fa-medium" style="padding:10px;"></i> </a>
-      <a href="https://www.youtube.com/c/ether1" target="_blank" class="social-link"> <i class="fa fa-youtube" style="padding:10px;"></i> </a>
-      <a href="https://discordapp.com/invite/MFn9Tmz" target="_blank" class="social-link"> <i class="fa fa-discord" style="padding:10px;"></i> </a>
+      <a href="https://www.youtube.com/channel/UC3nvSLrJq3m32v_oVHLwFng/" target="_blank" class="social-link"> <i class="fa fa-youtube" style="padding:10px;"></i> </a>
+      <a href="https://discordapp.com/invite/MFn9Tmz" target="_blank" class="social-link"> <i class="fa fa-commenting" style="padding:10px;"></i> </a>
     </center>
     <p>© 2021 The ETHO Protocol | Ether-1 All Rights Reserved.</p>
   </div>
